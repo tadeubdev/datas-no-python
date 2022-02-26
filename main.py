ecb19d665792c8d94acbb0d82ffaa2d0d66fd888
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-26 10:41:40 -0300

    feat: adiciona timezone ao arquivo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
-data_atual = datetime.today()
+diferenca = timedelta(hours=-3)
+fuso_horario = timezone(diferenca)
+
+data_atual = datetime.today().astimezone(fuso_horario)
 print(data_atual)
 
 data_em_texto = '{}/{}/{}'.format(data_atual.day, data_atual.month, data_atual.year)
@@ -12,6 +15,6 @@ print(data_formatada)
 data_e_hora_atuais = data_atual.strftime('%d/%m/%Y %H:%Mh')
 print(data_e_hora_atuais)
 
-data_e_hora_em_texto = '26/02/2022 10:34';
+data_e_hora_em_texto = '26/02/2022 10:34'
 data_e_hora_em_texto_formatada = datetime.strptime(data_e_hora_em_texto, '%d/%m/%Y %H:%M')
 print(data_e_hora_em_texto_formatada)
