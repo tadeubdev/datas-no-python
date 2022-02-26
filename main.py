e47b773f68200e384f734845e779fef8a32d4c05
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-26 10:29:14 -0300

    feat: adiciona data por extenso

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from datetime import date
 
 data_atual = date.today()
 print(data_atual)
+
+data_em_texto = '{}/{}/{}'.format(data_atual.day, data_atual.month, data_atual.year)
+print(data_em_texto)
